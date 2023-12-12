86a1e10427fd137a8bf27631be9748a01d0e0013
farhanariyandi <129134089+farhanariyandi@users.noreply.github.com>
2023-12-12 22:51:32 +0700

    F06 Halaman Search User

--- user/script/search.js
+++ user/script/search.js
@@ -1,36 +1,61 @@
-$(function () {
-    $.ajax({
-        type: 'GET',
-        url:host_be+'read_produk.php',
-        dataType: 'json',
-        success: function (data) {
-            if (data.status === 200) {
-                var books = data.body.data;
+$(document).ready(function () {
+    function updateUrl(searchTerm) {
+        var newUrl = window.location.pathname + '?cari=' + encodeURIComponent(searchTerm);
+        window.history.pushState({ path: newUrl }, '', newUrl);
+    }
 
-                // Iterate through each book in the array
-                $.each(books, function (index, book) {
-                    var judul = book.judul_bk;
-                    var cover = book.cover_bk;
-                    var kategori = book.nama_kategori;
-                    var truncatedSinop = book.sinop_bk.length > 200 ? book.sinop_bk.substring(0, 200) + "..." : book.sinop_bk;
-                    // Append HTML content for each book
-                    $('#hasil-cari').append(
-                        '<div class="col-lg-4 konten1 d-flex justify-content-center">' +
-                        '<img src="img/' + cover + '" alt="">' +
-                        '</div>' +
-                        '<div class="col-md-8 konten1">' +
-                        '<p><strong><a href="" style="color: inherit;text-decoration: none;">' + judul + '</a></strong>'+
-                        '<br>' + kategori + '<br><br><br>' + truncatedSinop + '</p>' +
-                        '</div>'
-                    );
-                });
-            } else {
-                // Handle error case
-                console.error("Error: " + data.msg);
-            }
-        },
-        error: function (xhr, status, error) {
-            console.error("AJAX Error: " + status + " - " + error);
+    // Attach an event handler to the form with ID "searchForm"
+    $('#searchForm').submit(function (event) {
+        event.preventDefault();
+        var newSearchTerm = $('#searchInput').val();
+
+        if (newSearchTerm || newSearchTerm.trim() === "") {
+            // Update the URL with the new search term
+            updateUrl(newSearchTerm);
+
+            // Perform the AJAX request
+            $.ajax({
+                type: 'GET',
+                url: host_be+'cari.php',
+                data: { cari: newSearchTerm },
+                dataType: 'json',
+                success: function (response) {
+                    console.log('Success: ', response);
+
+                    if (response && response.status === 200 && response.body && response.body.data) {
+                        console.log('Data: ', response.body.data);
+
+                        // Assuming response.body.data contains the array of search results
+                        $('#hasil-cari').empty(); // Clear existing content
+
+                        // Loop through the data and append to the container
+                        for (var i = 0; i < response.body.data.length; i++) {
+                            var item = response.body.data[i];
+                            var newItem = $('<div class="col-lg-4 konten1 d-flex justify-content-center">' +
+                                '<img src="../assets/img/' + item.cover_bk + '">' +
+                                '</div>' +
+                                '<div class="col-lg-8 konten1">' +
+                                '<p><strong><a href="?page=produk&isbn='+item.isbn_bk+'" style="color: inherit;text-decoration: none;">' + item.judul_bk + '</a></strong><br>' + 
+                                item.nama_kategori + '<br><br><br>' +
+                                item.sinop_bk +
+                                '</p>' +
+                                '</div>');
+
+                            $('#hasil-cari').append(newItem);
+                        }
+                    } else {
+                        console.error('Invalid response format or status code.');
+                    }
+                },
+                error: function (error) {
+                    console.error('Error:', error);
+                }
+            });
+        } else {
+            // Handle the case when the search term is empty
+            // For example, you can load all data here
+            console.log('Empty search term. Loading all data...');
+            // Make an AJAX request or load all data using a different method
         }
     });
 });
