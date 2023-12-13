063a7ac33f8b6610c5f986945bbca2ac334ac171
Dhooop <muhammadridho2271@gmail.com>
2023-12-13 23:16:54 +0700

    f04_halaman_edit_admin

--- admin/modul/edit.php
+++ admin/modul/edit.php
@@ -1,99 +1,45 @@
- <!-- konten -->
- <div class="mx-5">
-          <h3 class="mt-5">Edit data buku e-Perpus</h3>
-          <p>perubahan data buku pada e-perpus</p>
-          <hr class="bg-dark ">
+<?php
+$respon = [
+    "status" => 200,
+    "msg" => "",
+    "body" => [
+        "data" => []
+    ]
+];
 
-          <!-- form -->
-          <div class="col">
-            <div class="container-fluid">
-              <div class="row mt-5">
-                <div class="col">
+$koneksi = mysqli_connect("localhost", "root", "", "e-perpus");
 
-                  <table class="table" style="border-style: solid;">
-                    <thead>
-                      <tr style="border-style: solid;">
-                        <th>form</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>
-
-                          <div class="container">
-                            <form action="/action_page.php">
-                            <div class="row">
-                              <div class="col-25">
-                                <label for="fname">Cover</label>
-                              </div>
-                              <div class="col-75">
-                                <input type="text" id="fname" name="firstname" value="Filosofi Teras.jpg">
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-25">
-                                <label for="fname">Judul</label>
-                              </div>
-                              <div class="col-75">
-                                <input type="text" id="fname" name="firstname" value="Filosofi Teras">
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-25">
-                                <label for="lname">ISBN</label>
-                              </div>
-                              <div class="col-75">
-                                <input type="text" id="lname" name="lastname" value="1004">
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-25">
-                                <label for="country">Kategori</label>
-                              </div>
-                              <div class="col-75">
-                                <select id="country" name="country">
-                                  <option value="australia">Pendidikan</option>
-                                  <option value="canada">Cerpen</option>
-                                  <option value="usa">Novel</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-25">
-                                <label for="subject">Sinopsis</label>
-                              </div>
-                              <div class="col-75">
-                                <textarea id="sinopsis" name="sinopsis" style="height:100px">Buku Filosofi Teras menceritakan tentang Henry Manampiring, seorang praktisi periklanan dan penulis buku Filosofi Teras. Pada pertengahan tahun 2017, Henry menghadapi masalah mental, termasuk negative thinking dan depresi, yang kemudian didiagnosis sebagai Major Depressive Disorder oleh seorang psikiater.</textarea>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-25">
-                                <label for="lname">File</label>
-                              </div>
-                              <div class="col-75 input-group">
-                                <input type="text" id="lname" class="form-control" name="file" style="height: 40px;" value="filosofi teras.pdf">
-                                <div class="input-group-append">
-                                  <button class="btn text-white" style="height: 40px; background-color: #2A3F54;" type="button">select file</button>
-                                </div>
-                              </div>
-                            </div>
-                            <br>
-                            <div class="row float-right">
-                              <button class="btn text-white mt-1" type="button" style="background-color: #2A3F54;"><a style="color: #1ABB9C">Tambahkan</a></button>
-                            </div>
-                            </form>
-                          </div>
+if(isset($_POST['submit'])) {
+    $judul = $_POST['judul_bk'];
+    $isbn = $_POST['isbn_bk'];
+    $kategori = $_POST['nama_kategori'];
+    $sinopsis = $_POST['sinop_bk'];
+    $cover = $_FILES['cover_bk'];
+    $coverDir = "../asset/img/";
+    $targetcover = $coverDir . basename($_FILES["cover_bk"]["name"]);
+    move_uploaded_file($_FILES["cover_bk"]["tmp_name"], $targetcover);
+    $file_bk = $_FILES['file_bk'];
+    $fileDir = "../asset/img";
+    $targetFile = $fileDir . basename($_FILES["file_bk"]["name"]);
+    move_uploaded_file($_FILES["file_bk"]["tmp_name"], $targetFile);
 
 
-                   
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+    $sql = "UPDATE data_buku set judul_bk='$judul', nama_kategori='$kategori', sinop_bk='$sinopsis',
+            cover_bk='$cover', file_bk='$file_bk' where isbn_bk='$isbn'";
+            header("location:index.html");
 
-            </div>
-            </tbody>
-          </table>
-        </div>
-        </div>
-      </div>
+    if ($conn->query($sql) === TRUE) {
+        echo "Data berhasil ditambahkan";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+echo json_encode($respon);
+
+
+
+
+
+
+$koneksi->close();
+?>
