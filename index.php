69a5a995914f203d2497661810f7a1316365c6ea
soniavrnk <129132608+soniavrnk@users.noreply.github.com>
2024-01-03 08:32:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <script src="env.js"></script>
     <title>Kategori</title>
   </head>
   <body>
